8ba31f601c0a1a0b0097651103a58a917593aa34
clinton-otong <clinton_otong@yahoo.com>
2022-06-30 14:54:51 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-#include <string.h>
- 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
- 
-int main(void)
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	 
 	return (0);
 }
 
