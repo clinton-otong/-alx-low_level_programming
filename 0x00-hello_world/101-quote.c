a90cc46eb6a08599cc6534b1401281b5f88794c2
clinton-otong <clinton_otong@yahoo.com>
2022-06-30 17:40:12 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  *  * main - prints a standard error
  *   
  *   *
- *    
+ *
  *    * Return: Always 1 (Success)
  *     
  *     *
@@ -14,13 +14,13 @@ int main(void)
 	 
 {
 	 
-	fwrite(2,"and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
+        printf(2,"and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
 	return (1);
 	 
 }
 
  
-       fprintf(stderr,"and that piece of art is useful\" - \
+       printf(stderr,"and that piece of art is useful\" - \
 		Dora Korpar,2015-10-19\n");
        return (1);
 }
