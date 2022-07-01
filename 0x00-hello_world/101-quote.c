124c703a6ff43e2c066c15e824e43d7bdad95d8f
clinton-otong <clinton_otong@yahoo.com>
2022-07-01 06:18:33 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ", 58);
-	write(0, "\n", 1);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", str);
 	return (1);
 }
