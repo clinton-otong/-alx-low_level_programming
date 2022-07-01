467bdbe328bbe6834cf847ffed8a62857bb3d023
clinton-otong <clinton_otong@yahoo.com>
2022-06-30 17:30:41 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
  *  
  *  * main - prints a standard error
@@ -15,15 +14,13 @@ int main(void)
 	 
 {
 	 
-	write(2,"and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
-	 
+	fwrite(2,"and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
 	return (1);
 	 
 }
 
  
-fprintf(stderr,"and that piece of art is useful\" - \
+       fprintf(stderr,"and that piece of art is useful\" - \
 		Dora Korpar,2015-10-19\n");
- 
-return (1);
+       return (1);
 }
